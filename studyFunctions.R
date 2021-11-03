72a0aa5aaa13a6835faa6d8d452926b036d6325c
Daniel Garza <danielriosgarza@gmail.com>
2021-11-03 13:13:35 +0100

    Update studyFunctions.R

--- studyFunctions.R
+++ studyFunctions.R
@@ -12,6 +12,12 @@ installIfNeeded("ggplot2", "deSolve", "reshape2", "Matrix",
                 "mvtnorm", "pracma", "gtools", "maotai", "umap", "BiodiversityR")
 
 
+setwd("C:/Users/u0139894/Documents/GitHub/microbialTimeSeries")
+
+Rfiles = gsub(" ", "", paste("./R/", list.files("./R")))
+sapply(Rfiles, source)
+
+
 getMoments <- function(simulaionMatrix, is.perCapita = FALSE){
   
   simul <- simulaionMatrix
