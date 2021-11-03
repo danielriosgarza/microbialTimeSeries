cd76c00d7f9270aa894849461a4e312cf725a22e
Daniel Garza <danielriosgarza@gmail.com>
2021-11-03 06:04:24 +0100

    Add files via upload

---
+++ model_params.R
@@ -0,0 +1,158 @@
+################Load all package functions#################
+library(ggplot2)
+library(deSolve)
+library(reshape2)
+library(Matrix)
+library(mvtnorm)
+library(pracma)
+library(gtools)
+library(maotai)
+
+setwd(dir = "C:/Users/u0139894/Documents/microsimPy")
+
+Rfiles = gsub(" ", "", paste("./R/", list.files("./R")))
+sapply(Rfiles, source)
+
+
+##############Simulation conditions###################################
+
+t.start = 0
+t.end = 2000
+t.step = 0.05
+t.store = 500
+norm = FALSE
+
+simul.cond = list(t.start = t.start, t.end = t.end, t.step = t.step, t.store = t.store, norm = norm)
+
+
+
+###############Common parameters#####################################
+n.species = 25
+migration.p = 0.1 
+metacommunity.eveness = 1 #vary from 1 (uneven) to 1000 (highly even)
+metacommunity.p = rdirichlet(1, rep(.5, n.species)*metacommunity.eveness)
+growth.rates <- rbeta(n.species, shape1 = 0.5, shape2 = 0.5)
+#growth.rates <- rep(1, n.species) 
+x0 <- rep(0.001, n.species)
+self.interactions <- runif(n.species, max(growth.rates)/3, max(growth.rates))
+
+##############Stochastic ODE Models###################################
+stochastic = FALSE
+sigma.drift = 0.01
+sigma.epoch = 0.1
+sigma.external = 0.3
+sigma.migration = 0.3
+p.epoch = .1
+n.external.perturbation = 1
+t.external_events = getPerturbT(t.end, n.external.perturbation)
+t.external_durations = rep(1, n.external.perturbation)
+
+stoch.cond <- list(stochastic=stochastic, sigma.drift = sigma.drift,
+                   sigma.epoch = sigma.epoch, sigma.external = sigma.external,
+                   sigma.migration = sigma.migration, p.epoch = p.epoch, 
+                   t.external_events = t.external_events,
+                   t.external_durations = t.external_durations)
+
+
+###############For the per capita based models#####################################
+n.individuals = 1000 
+community.initial = rmultinom(1, n.individuals, rdirichlet(1, rep(1, n.species))) [,]
+k.events = 1
+
+######################Params Hubbel Model ########################
+
+
+
+hub_params = c(simul.cond, list(community.initial = community.initial, migration.p = migration.p,
+                                metacommunity.p = metacommunity.p, k.events = k.events))
+
+hub <- parse(text = "do.call(simulateHubbell, hub_params)")
+
+
+
+######################Params Hubbel Rates Model ########################
+
+
+hub_rates_params = c(simul.cond, list(community.initial = community.initial, migration.p = migration.p,
+                                  metacommunity.p = metacommunity.p, k.events = k.events,
+                                  growth.rates = growth.rates))
+
+
+hub_rates <- parse(text = "do.call(simulateHubbellRates, hub_rates_params)")
+
+
+######################Params Logistic Model ########################
+carrying.k <- growth.rates/self.interactions
+death.rates = rep(0.001, n.species)
+
+
+slm_params <- c(stoch.cond, simul.cond, list(n.species = n.species, growth.rates = growth.rates,
+                   carrying.k = carrying.k, death.rates = death.rates,
+                   x0 = x0, migration.p=migration.p ))
+
+slm <- parse(text = "do.call(simulateStochasticLogistic, slm_params)")
+
+######################Params GLV Model ########################
+
+####interaction matrix####
+symmetric = FALSE
+connectance = 0.5
+diagonal = -1*self.interactions
+mutualism.w = 1
+commensalism.w = 1
+parasitism.w = 1
+amensalism.w = 1
+competition.w = 1
+interaction.w <- c(mutualism.w, commensalism.w, parasitism.w, amensalism.w,  competition.w)
+scale = 0.5
+sigma.A = 1
+distribution = rnorm(n=n.species^2, sd = sigma.A)
+
+
+rA_params <- list(n.species = n.species, connectance = connectance, 
+                  diagonal = diagonal, interaction.w = interaction.w,
+                  scale = scale, distribution = distribution, symmetric = symmetric)
+
+A = eval(parse(text = "do.call(randomA, rA_params)"))
+##########################
+
+glv_params <-  c(stoch.cond, simul.cond, list(n.species = n.species, A=A, 
+                                              growth.rates = growth.rates,
+                                              x0 = x0, migration.p=migration.p ))
+
+
+glv <- parse(text = "do.call(simulateGLV, glv_params)")
+
+######################Params Consumer Resource Model ########################
+
+######Consumption/production matrix######
+n.resources = as.integer(1.3*n.species)
+consumption.w = 0 
+production.w = 1
+maintenance = 0
+
+mean.prod = production.w * n.resources
+mean.con = consumption.w * n.resources
+
+
+rE_params <- list(n.species = n.species, n.resources = n.resources, 
+                  mean.prod = mean.prod, mean.con = mean.con, maintenance = maintenance)
+
+E = eval(parse(text= 'do.call(randomE, rE_params)'))
+
+###########################################
+resource.eveness = 1
+resource_dist = rdirichlet(1, rep(.5, n.resources)*resource.eveness)
+resource_concentration = 1000
+resources = resource_dist*resource_concentration
+k.mul = 50
+monod.k <- matrix(rgamma(n=n.species*n.resources, shape = k.mul*max(resources),
+              rate = 1), nrow = n.species)
+
+
+crm_params <- c(simul.cond, list(n.species = n.species, n.resources = n.resources, 
+                                eff=E, x0=x0, resources = resources, 
+                                growth.rates = growth.rates, monod.k = monod.k))
+
+crm <- parse(text = "do.call(simulateConsumerResource, crm_params)")
+
---
+++ studyFunctions.R
@@ -0,0 +1,73 @@
+library(ggplot2)
+library(deSolve)
+library(reshape2)
+library(Matrix)
+library(mvtnorm)
+library(pracma)
+library(gtools)
+library(maotai)
+
+
+getMoments <- function(simulaionMatrix, is.perCapita = FALSE){
+  
+  simul <- simulaionMatrix
+  
+  if (is.perCapita){
+    
+    S = sum(simul[1,])
+    
+    meanH <- colMeans(simul)
+    covH <- cov(simul)
+    m2 = 2*(1/S)*(1/(S-1))*t(apply(covH, 1, cumsum))
+    
+    diag(m2) <- (1/S)*diag(covH)
+    
+    return(list(m1 = (1/S)*meanH, m2 = (1/S)*covH, basis = simulaionMatrix))
+  }
+  
+  return(list(m1 = colMeans(simul), m2 = cov(simul), basis = simulaionMatrix))
+  
+  
+}
+
+
+generateMoments <- function(modelGenerateExp, n.instances, t.store, is.perCapita=FALSE){
+
+  modelMatrix <- eval(modelGenerateExp)$matrix
+  
+  simul <- modelMatrix[,colnames(modelMatrix)!="time"]
+  
+  summaryMatrix <- matrix(0, nrow = n.instances, ncol = ncol(simul))
+  
+  for (i in 1:n.instances){
+    print(i)
+    modelMatrix <- eval(modelGenerateExp)$matrix
+    
+    simul <- modelMatrix[,colnames(modelMatrix)!="time"]
+    
+    summaryMatrix[i,] <- simul[t.store,]
+    
+  }
+  
+  return(getMoments(summaryMatrix, is.perCapita = is.perCapita))
+  
+  
+  
+}
+
+
+getKL <- function(mA, mB){
+  mBinv = pinv(mB$m2)
+  detA = as.numeric(unlist(pdeterminant(mB$m2))[1])
+  detB = as.numeric(unlist(pdeterminant(mA$m2))[1])
+  
+  return (max(0, (1/2)*( (detA - detB) - 
+                    length(mA$m1) + 
+                    sum(diag(mBinv%*%mA$m2)) + 
+                    (mB$m1 - mA$m1)%*%mBinv%*%matrix(mB$m1 - mA$m1))))
+}
+
+getPerturbT <- function(endTime, n.perturbs){
+  st <- SimulationTimes(t.end = endTime, t.store = n.perturbs + 1)
+  return (st$t.sys[st$t.index[2:length(st$t.index)]])
+}
