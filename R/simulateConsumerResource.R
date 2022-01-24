1d8af50b219125301d07fece323432fda78b3e8a
Yu Gao <gaoyu19920914@gmail.com>
2022-01-24 10:13:57 +0100

    Update simulateConsumerResource.R
    add comments/descriptions for perturbation-related variables

--- R/simulateConsumerResource.R
+++ R/simulateConsumerResource.R
@@ -28,6 +28,40 @@
 #' `matrix(rgamma(n = n.species*n.resources, shape = 50*max(resources), rate = 1), nrow = n.species)`
 #' is used.
 #' (default: \code{monod.constant = NULL})
+#' @param sigma.drift Numeric: standard deviation of a normally distributed 
+#' noise applied in each time step (t.step)
+#' (default: \code{sigma.drift = 0.001})
+#' @param sigma.epoch Numeric: standard deviation of a normally distributed 
+#' noise applied to random periods of the community composition with frequency 
+#' defined by the epoch.p parameter
+#' (default: \code{sigma.epoch = 0.1})
+#' @param sigma.external Numeric: standard deviation of a normally distributed 
+#' noise applied to user-defined external events/disturbances
+#' (default: \code{sigma.external = 0.3})
+#' @param sigma.migration Numeric: standard deviation of a normally distributed 
+#' variable that defines the intensity of migration at each time step (t.step)
+#' (default: \code{sigma.migration = 0.01})
+#' @param epoch.p Numeric: the probability/frequency of random periodic 
+#' changes introduced to the community composition
+#' (default: \code{epoch.p = 0.001})
+#' @param t.external_events Numeric: the starting time points of defined 
+#' external events that introduce random changes to the community composition
+#' (default: \code{t.external_events = NULL})
+#' @param t.external_durations Numeric: respective duration of the external 
+#' events that are defined in the 't.external_events' (times) and 
+#' sigma.external (std).
+#' (default: \code{t.external_durations = NULL})
+#' @param stochastic Logical: whether to introduce noise in the simulation.
+#' If False, sigma.drift, sigma.epoch, and sigma.external are ignored.
+#' (default: \code{stochastic = TRUE})
+#' @param migration.p Numeric: the probability/frequency of migration from a 
+#' metacommunity.
+#' (default: \code{migration.p = 0.01})
+#' @param metacommunity.probability Numeric: Normalized probability distribution
+#' of the likelihood that species from the metacommunity can enter the community
+#' during the simulation. If NULL, `rdirichlet(1, alpha = rep(1,n.species))` is 
+#' used.
+#' (default: \code{metacommunity.probability = NULL})
 #' @param error.variance Numeric: the variance of measurement error.
 #' By default it equals to 0, indicating that the result won't contain any 
 #' measurement error. This value should be non-negative.
@@ -155,7 +189,7 @@ simulateConsumerResource <- function(n.species, n.resources,
             
             #consumption <- (t(growth) %*% ((E>0)/B))*resources
             #production <- -(t(growth) %*% (E*(E<0)/B))*resources
-            dResources <- consumption + production - dilution.rate*(resources - as.vector(resources.dilution))
+            dResources <- consumption + production - dilution.rate*(resources - resources.dilution)
             dConsumers <- growth.rates*growth - dilution.rate*x0
             dxdt <- list(c(dConsumers, dResources))
             return(dxdt)
